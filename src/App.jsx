9ee9694ac2fef1a26052b23b71b316f8222ff7cd
Chuksy25 <149021806+Chuksy25@users.noreply.github.com>
2023-10-26 00:14:47 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useGesture } from "@use-gesture/react";
+import "./App.css";
+
+const images = [
+  "/images/Nft-1.jpg",
+  "/images/Nft-2.png",
+  "/images/Nft-3.jpg",
+  "/images/Nft-4.jpg",
+  "/images/Nft-5.jpg",
+  "/images/Nft-6.jpg",
+  "/images/Nft-7.png",
+  "/images/Nft-8.jpg",
+];
+
+const App = () => {
+  const [position, setPosition] = useState([0, 0]);
+  const [hovered, setHovered] = useState(false);
+
+  const handlers = useGesture({
+    onDrag: ({ offset: [x, y] }) => {
+      setPosition([x, y]);
+    },
+    onWheel: ({ delta: [, dy] }) => {
+      // Implement your logic for zooming or other actions with the wheel event
+      // For example, you can adjust the scale of the component
+      // based on the wheel scroll direction (dy).
+      // Here's a simple zoom example:
+      setPosition((prevPosition) => [prevPosition[0], prevPosition[1] - dy]);
+    },
+    onHover: ({ hovering }) => {
+      setHovered(hovering);
+    },
+  });
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        {images.map((image, index) => (
+          <div key={index} className="image-container" {...handlers()}>
+            <img
+              style={{
+                transform: `translate(${position[0]}px, ${position[1]}px)`,
+                backgroundColor: hovered ? "lightblue" : "white", // Change background color on hover
+              }}
+              src={image}
+              alt={`Image ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
